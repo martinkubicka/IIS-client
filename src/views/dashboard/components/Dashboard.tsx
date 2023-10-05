88b7bdb29109ee82b733205875675823572a6f00
xmacek27 <matej.macek@email.cz>
2023-10-05 23:56:03 +0200

    Add threads to dashboard

--- src/views/dashboard/components/Dashboard.tsx
+++ src/views/dashboard/components/Dashboard.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Page } from "@src/shared/components/Page";
 import GroupComponent from "./GroupComponent";
-import { Divider } from "@mui/joy";
+import { Divider, Typography } from "@mui/joy";
 import { groupService } from "@src/services/groupService";
 import { GroupModel } from "@src/shared/models/GroupModel";
 import { Thread } from "@src/shared/components/Threads";
@@ -12,17 +12,16 @@ export const Dashboard = () => {
   const [userGroups, setUserGroups] = useState<GroupModel[]>([]);
   const [recommendedGroups, setRecommendedGroups] = useState<GroupModel[]>([]);
   const [threads, setThreads] = useState<ThreadModel[] | null>(null);
-  const [memberIcons, setMemberIcons] = useState([]);
 
   const onDelete = async () => {
     setThreads(null);
   };
 
   useEffect(() => {
-    //todo  Replace 'userEmail' with the actual user's email address
+    // todo: Replace 'userEmail' with the actual user's email address
     const userEmail = "user1@example.com";
 
-    const fetchUserGroups = async () => {
+    const fetchUserGroupsAndThreads = async () => {
       try {
         // Fetch user's joined groups
         const joinedGroups = await groupService.getGroupsUserIsIn(
@@ -37,17 +36,23 @@ export const Dashboard = () => {
           false
         );
         setRecommendedGroups(recommendedGroups.slice(0, 5)); // Only get the first 5 recommended groups
+
+        // Fetch all threads the user is in
+        const allThreads = await threadService.getAllThreadsUserIsIn(userEmail);
+        setThreads(allThreads); // Set the threads in the state
       } catch (error) {
-        console.error("Error fetching user groups:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
-    fetchUserGroups();
+    fetchUserGroupsAndThreads();
   }, []);
 
   return (
     <Page>
-      <h1>My Groups</h1>
+      <Typography level="h1" fontSize="x2" sx={{ mb: 0.5 }}>
+        My Groups
+      </Typography>
       <div style={{ display: "flex", gap: "16px" }}>
         {userGroups.map((group, index) => (
           <GroupComponent
@@ -65,8 +70,9 @@ export const Dashboard = () => {
       <div style={{ marginTop: "20px" }}>
         <Divider />
       </div>
-
-      <h1>Suggested</h1>
+      <Typography level="h1" fontSize="x2" sx={{ mb: 0.5 }}>
+        Suggested
+      </Typography>
       <div style={{ display: "flex", gap: "16px" }}>
         {recommendedGroups.map((group, index) => (
           <GroupComponent
@@ -85,13 +91,20 @@ export const Dashboard = () => {
         <Divider />
       </div>
 
-      {threads === null
-        ? "Loading threads..."
-        : threads.length === 0
-        ? "No threads found"
-        : threads.map((thread, index) => (
+      {threads === null ? (
+        "Loading threads..."
+      ) : threads.length === 0 ? (
+        "No threads found"
+      ) : (
+        <div>
+          <Typography level="h1" fontSize="x2" sx={{ mb: 0.5 }}>
+            Threads
+          </Typography>
+          {threads.map((thread, index) => (
             <Thread key={index} thread={thread} onDelete={onDelete} />
           ))}
+        </div>
+      )}
     </Page>
   );
 };
