2eb740b98b6262d7564ed4808b2f0eb218977f7b
xmacek27 <matej.macek@email.cz>
2023-10-07 20:32:07 +0200

    Add userService

---
+++ src/services/userService.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import API_BASE_URL from "@src/apiConfig";
+import { UserDetailModel } from "@src/shared/models/UserDetailModel";
+import { UserPrivacySettingsModel } from "@src/shared/models/UserPrivacySettingsModel";
+
+const instance = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export const userService = {
+  async updateUser(
+    updatedUser?: UserDetailModel,
+    userPrivacy?: UserPrivacySettingsModel
+  ) {},
+
+  async getUser(handle?: string) {
+    try {
+      const response = await instance.get(`/User/profile?handle=${handle}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async getPrivacy(handle?: string) {
+    try {
+      const response = await instance.get(`/User/privacy?handle=${handle}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteUser(email?: string) {
+    try {
+      const response = await instance.delete(`/User/delete?email=${email}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
