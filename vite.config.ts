3b94f19f51d65f1c18768e650520188eafc62c84
Martin Kubička <94179490+martinkubicka@users.noreply.github.com>
2023-11-19 12:48:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    {
-      name: 'custom-mime-type',
-      transform(code, id) {
-        if (id.endsWith('.tsx')) {
-          return {
-            code,
-            map: null,
-            mimeType: 'application/javascript'
-          };
-        }
-      }
-    }
-  ]
+  plugins: [react(), tsconfigPaths()]
 });
