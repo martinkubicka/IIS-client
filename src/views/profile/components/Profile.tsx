9934bf47c10b2647a65e701659ab60f7a764d74b
xmacek27 <matej.macek@email.cz>
2023-11-24 23:11:27 +0100

    Fix Privacy on profile

--- src/views/profile/components/Profile.tsx
+++ src/views/profile/components/Profile.tsx
@@ -1,19 +1,16 @@
 // Profile.tsx
 import { Page } from "@src/shared/components/Page";
-import { Avatar, Skeleton, Button } from "@mui/joy";
+import { Avatar, Skeleton } from "@mui/joy";
 import { Icon } from "@src/shared/components/Icon/Icon";
 import TabMenu from "./TabMenuProfile";
 import PermissionDenied from "./PermissionDenied";
-
 import { useParams } from "react-router-dom";
-
 import { UserDetailModel } from "@src/shared/models/UserDetailModel";
 import { useEffect, useState } from "react";
 import { userService } from "@src/services/userService";
 import { PageHeader } from "@src/shared/components/PageHeader";
 import { UserPrivacySettingsModel } from "@src/shared/models/UserPrivacySettingsModel";
 import { loginService } from "@src/services/loginService";
-import { Link } from "react-router-dom";
 import { groupService } from "@src/services/groupService";
 
 export const Profile = () => {
@@ -32,6 +29,7 @@ export const Profile = () => {
   const userHandle = loginService.getCookie("userHandle") || "";
   const [memberPrivacy, setMemberPrivacy] =
     useState<UserPrivacySettingsModel | null>(null);
+
   const onSettingsSaved = async () => {
     const updatedUserData = await userService.getUser(handle);
     const updatedUserDetailDataResponse: UserDetailModel = {
@@ -50,76 +48,96 @@ export const Profile = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // not registered user
         if (handleMember.handle) {
           const memberPrivacyDataResponse = await userService.getPrivacy(
             handleMember.handle
           );
           setMemberPrivacy(memberPrivacyDataResponse);
-        }
 
-        if (handleMember && handleMember.handle !== userHandle) {
-          if (memberPrivacy?.visibilityGuest === false && userHandle === "") {
-            setShowRestrictedContent(true);
-            setErrorMessage(
-              "Sorry, you don't have permission to view this profile. Only registered accounts have access to this profile."
-            );
-          }
+          if (handleMember && handleMember.handle !== userHandle) {
+            if (memberPrivacy?.visibilityGuest === false && userHandle === "") {
+              setShowRestrictedContent(true);
+              setErrorMessage(
+                "Sorry, you don't have permission to view this profile. Only registered accounts have access to this profile."
+              );
+            }
 
-          if (
-            memberPrivacy?.visibilityRegistered === false &&
-            userHandle !== ""
-          ) {
-            setShowRestrictedContent(true);
-            setErrorMessage(
-              "Sorry, you don't have permission to view this profile. Only registered accounts have access to this profile."
-            );
-          }
+            if (
+              memberPrivacy?.visibilityRegistered === false &&
+              userHandle !== "" &&
+              showRestrictedContent === false
+            ) {
+              setShowRestrictedContent(true);
+              setErrorMessage(
+                "Sorry, you don't have permission to view this profile. The registred accounts doesn't have access to this profile."
+              );
+            }
 
-          if (memberPrivacy?.visibilityGroup === false) {
-            if (userHandle !== "") {
-              try {
-                const userGroups = await groupService.getGroupsUserIsInByHandle(
-                  userHandle
-                );
-                const profileUserGroups =
-                  await groupService.getGroupsUserIsInByHandle(
-                    handleMember.handle
+            if (
+              memberPrivacy?.visibilityGroup === false &&
+              showRestrictedContent === false
+            ) {
+              if (userHandle !== "") {
+                try {
+                  const userGroups =
+                    await groupService.getGroupsUserIsInByHandle(userHandle);
+                  const profileUserGroups =
+                    await groupService.getGroupsUserIsInByHandle(
+                      handleMember.handle
+                    );
+
+                  const hasCommonGroup = userGroups.some((userGroup) =>
+                    profileUserGroups.some(
+                      (profileGroup) => userGroup.handle === profileGroup.handle
+                    )
                   );
 
-                const hasCommonGroup = userGroups.some((userGroup) =>
-                  profileUserGroups.some(
-                    (profileGroup) => userGroup.handle === profileGroup.handle
-                  )
-                );
-
-                if (!hasCommonGroup) {
-                  setShowRestrictedContent(true);
-                  setErrorMessage(
-                    "Sorry, you don't have permission to view this profile. Only members of the same group have access to this profile."
-                  );
+                  if (!hasCommonGroup) {
+                    setShowRestrictedContent(true);
+                    setErrorMessage(
+                      "Sorry, you don't have permission to view this profile. Only members of the same group have access to this profile."
+                    );
+                  }
+                } catch (error) {
+                  console.error("Error fetching user groups:", error);
                 }
-              } catch (error) {
-                console.error("Error fetching user groups:", error);
+              } else {
+                setShowRestrictedContent(true);
+                setErrorMessage(
+                  "Sorry, you don't have permission to view this profile. Only Registred users who are in the same groups have access to this profile."
+                );
               }
             }
           }
         }
-        const userProfileDataResponse = await userService.getUser(handle);
-        const userDetailDataResponse: UserDetailModel = {
-          name: userProfileDataResponse.name,
-          icon: userProfileDataResponse.icon,
-          handle: userProfileDataResponse.handle,
-          role: userProfileDataResponse.role,
-          email: userEmail,
-        };
-        setUserDetailData(userDetailDataResponse);
 
-        const userPrivacyDataResponse = await userService.getPrivacy(handle);
-        setUserPrivacyData(userPrivacyDataResponse);
-        setIsLoading(false); // Move this line inside the try block just before the function ends
+        if (showRestrictedContent === false) {
+          const userProfileDataResponse = await userService.getUser(handle);
+          const userDetailDataResponse: UserDetailModel = {
+            name: userProfileDataResponse.name,
+            icon: userProfileDataResponse.icon,
+            handle: userProfileDataResponse.handle,
+            role: userProfileDataResponse.role,
+            email: userEmail,
+          };
+          setUserDetailData(userDetailDataResponse);
+
+          const userPrivacyDataResponse = await userService.getPrivacy(handle);
+          setUserPrivacyData(userPrivacyDataResponse);
+        }
+
+        // wait 2 sec and then set isLoading to false becouse we dont want to show user without permission to see profile
+        const timer = setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+
+        return () => clearTimeout(timer);
       } catch (error) {
         console.log(error);
-        setIsLoading(false); // Move this line inside the try block just before the function ends
+        setShowRestrictedContent(true);
+        setErrorMessage("Sorry, some problems come when loading profile.");
+        setIsLoading(false);
       }
     };
 
