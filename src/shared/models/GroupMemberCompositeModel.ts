34b98056c2e96b4963387cbc3dd43e12381a8c58
xmacek27 <matej.macek@email.cz>
2023-12-03 14:12:21 +0100

    feat: add doc headers

--- src/shared/models/GroupMemberCompositeModel.ts
+++ src/shared/models/GroupMemberCompositeModel.ts
@@ -1,3 +1,10 @@
+/**
+ * @file GroupMemberCompositeModel.ts
+ * @author { Matěj Macek (xmacek27) }
+ * @date 17.12.2023
+ * @brief Definition of GroupMemberCompositeModel interface
+ */
+
 import { GroupModel } from './GroupModel'; // Import your GroupModel interface or type
 import { MemberModel } from './MemberModel'; // Import your MemberModel interface or type
 
