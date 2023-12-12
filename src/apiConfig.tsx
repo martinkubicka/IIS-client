228d91823440b0660bc2910cf690dbf123b929e8
martinkubicka <martinkubicka22@gmail.com>
2023-12-12 10:59:41 +0100

    change api url

--- src/apiConfig.tsx
+++ src/apiConfig.tsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5203";
+const API_BASE_URL = "https://antioznuk-api-itu.azurewebsites.net";
 export const TENOR_API_KEY = "AIzaSyCoA1c1rVZb3qHX3HORc9DpCV0NGvXtaIM";
 
 export default API_BASE_URL;
